9035a14fb98a03825b985dccb0fbfd8736d84183
Kim Minh Thang <thangved02@gmail.com>
2023-02-11 13:20:39 +0700

    Finish

--- scripts.js
+++ scripts.js
@@ -47,8 +47,14 @@ function putPixel(px, py, color = "white") {
 function drawRealLine() {
 	context.strokeStyle = "#000";
 	context.beginPath();
-	context.moveTo(padding + poss.px1 * pWidth, padding + poss.py1 * pWidth);
-	context.lineTo(padding + poss.px2 * pWidth, padding + poss.py2 * pHeight);
+	context.moveTo(
+		padding + poss.px1 * pWidth + pWidth / 2,
+		padding + poss.py1 * pWidth + pHeight / 2
+	);
+	context.lineTo(
+		padding + poss.px2 * pWidth + pWidth / 2,
+		padding + poss.py2 * pHeight + pHeight / 2
+	);
 	context.stroke();
 }
 
@@ -70,6 +76,12 @@ const wait = (timeout) =>
 
 async function drawWidthLine(px1, px2) {
 	context.strokeStyle = "red";
+	const temp = px1;
+	if (px1 > px2) {
+		px1 = px2;
+		px2 = temp;
+	}
+
 	for (let i = px1; i < px2; i++) {
 		context.beginPath();
 		context.moveTo(padding + i * pWidth, padding / 2);
@@ -89,6 +101,13 @@ async function drawWidthLine(px1, px2) {
 
 async function drawHeightLine(py1, py2) {
 	context.strokeStyle = "red";
+
+	const temp = py1;
+	if (py1 > py2) {
+		py1 = py2;
+		py2 = temp;
+	}
+
 	for (let i = py1; i < py2; i++) {
 		context.beginPath();
 		context.moveTo(padding / 2, padding + i * pWidth);
@@ -110,8 +129,16 @@ async function start() {
 	initGrid();
 	drawRealLine();
 
-	for (let i = poss.px1; i < poss.px2; i++) {
-		for (let j = poss.py1; j < poss.py2; j++) {
+	for (
+		let i = Math.min(poss.px1, poss.px2);
+		i < Math.max(poss.px1, poss.px2);
+		i++
+	) {
+		for (
+			let j = Math.min(poss.py1, poss.py2);
+			j < Math.max(poss.py1, poss.py2);
+			j++
+		) {
 			putPixel(i, j, "yellow");
 		}
 		await wait(10);
@@ -121,22 +148,46 @@ async function start() {
 	await drawWidthLine(poss.px1, poss.px2);
 	await drawHeightLine(poss.py1, poss.py2);
 
-	const dxInc = Math.abs(poss.px1 - poss.px2);
+	const dxInc = poss.px2 - poss.px1;
+	const dyInc = poss.py2 - poss.py1;
+	const maxSteps = Math.max(Math.abs(dxInc), Math.abs(dyInc));
 
-	document.getElementById("dx-inc").innerText = dxInc;
+	const xStepWidth = dxInc / maxSteps;
+	const yStepWidth = dyInc / maxSteps;
+
+	document.getElementById("dx-inc").innerText = Math.abs(dxInc);
 
 	await wait(100);
 
-	const dyInc = Math.abs(poss.py1 - poss.py2);
-
-	document.getElementById("dy-inc").innerText = dyInc;
+	document.getElementById("dy-inc").innerText = Math.abs(dyInc);
 
 	await wait(100);
 
-	const maxSteps = Math.max(dxInc, dyInc);
+	document.getElementById("max-steps").innerText = Math.abs(maxSteps);
+	await wait(100);
 
-	document.getElementById("max-steps").innerText = maxSteps;
+	document.getElementById("x-step-width").innerText = Math.abs(
+		xStepWidth.toFixed(2)
+	);
+	await wait(100);
+
+	document.getElementById("y-step-width").innerText = Math.abs(
+		yStepWidth.toFixed(2)
+	);
 	await wait(10);
+
+	let xBegin = poss.px1;
+	let yBegin = poss.py1;
+
+	for (let i = 0; i < maxSteps; i++) {
+		putPixel(Math.round(xBegin), Math.round(yBegin), "orange");
+		xBegin += xStepWidth;
+		yBegin += yStepWidth;
+
+		drawRealLine();
+
+		await wait(100);
+	}
 }
 
 initGrid();
